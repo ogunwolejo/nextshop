5434869133ee575268ac2ff4e05dc5578375c3f8
joshua <ogunwole888@gmail.com>
2021-03-31 00:12:10 +0100

    connected to firebase and added the cloudfirestore dependency

---
+++ lib/utils/constant.dart
@@ -0,0 +1,13 @@
+import 'package:flutter/material.dart';
+
+const Map<String, Color> appColors = {
+  'k1': Color(0xff181820),
+  'k2': Color(0xff202028),
+  'k3': Color(0xff283038),
+  'r1': Color(0xffe84848),
+  'r2': Color(0xffe89088),
+  'r3': Color(0xffb03030),
+  'w1': Color(0xffffffff),
+  'w2': Color(0xffe8d8d8),
+  'b1': Color(0xffb8b8c0),
+};
